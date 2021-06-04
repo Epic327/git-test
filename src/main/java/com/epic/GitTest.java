ac87f48863ca6506ee291e74a8010ca2ece283e1
Eip <chengv327@163.com>
2021-06-04 15:53:10 +0800

    first commit

---
+++ src/main/java/com/epic/GitTest.java
@@ -0,0 +1,11 @@
+package com.epic;
+
+/**
+ * @author Epic
+ * @create 2021-06-04 15:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello  git");
+    }
+}
